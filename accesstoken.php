80c06f3bf2ec9fd71729f20d3fcde8813cd7486d
Alice Njonjo <ndutanjonjo19@gmail.com>
2024-05-11 10:53:36 +0300

    access token

---
+++ accesstoken.php
@@ -0,0 +1,18 @@
+<?php
+
+$consumerKey = "VzuACZOFyw3a7KuTvoNiYXL8z8d8f8iqdgpJsZlYnYGYnnSl";
+$consumerSecret = "n4sHr15xXiucloAbqeahKljK3jrJ6oKBHq5XyH8jXZ7wemh24fclEhkAsXeGcuau";
+
+$access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+$headers = ['Content-Type: application/json; charset=utf8'];
+$curl = curl_init($access_token_url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
+$result = curl_exec($curl);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+$result = json_decode($result);
+echo $access_token = $result->access_token;
+curl_close($curl);
+?>
